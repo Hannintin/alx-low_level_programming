dc3bd50d167fc5e611631f26a69617c285d340cd
Hannintin <hanscovic1@gmail.com>
2023-03-01 05:18:13 -0800

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * @str: the string
- * Return: the length of the string
+ * _puts - return the strings
+ *
+ * @str: return strings as inputted
  */
+
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
-	{
+	for (i = 0; str[i]; i++)
 		_putchar(str[i]);
-		i++;
-	}
 	_putchar('\n');
 }
