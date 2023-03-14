95ae69414a0dfed1a1e775daaf2c0987589d1113
Hannintin <hanscovic1@gmail.com>
2023-03-14 06:22:19 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - entry point
+ * @width: get the with for the matrics
+ * @height: get the height of the matrics
+ * Return: return the result
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int j, i;
+	int **arr;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	arr = (int **)malloc(sizeof(int *) * height);
+	if (arr == NULL)
+	{
+		free(arr);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
