69bf8d1c06458c7003bfc29c182e8380bc35620e
Hannintin <hanscovic1@gmail.com>
2023-04-04 06:59:04 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - main entry point to print linked list
+ *
+ * @h: get the integers to be printed in the nodes
+ *
+ * Return: Number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+
+	while (h != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
